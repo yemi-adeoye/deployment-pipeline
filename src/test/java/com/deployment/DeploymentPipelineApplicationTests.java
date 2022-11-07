b706a8d5e85ddbe62c419c022d7e97b59cc4449f
yemi-adeoye <adeyemi_adeoye@hotmail.com>
2022-11-07 13:39:31 -0600

    adds project files

---
+++ src/test/java/com/deployment/DeploymentPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploymentPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
